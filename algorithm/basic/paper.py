e753afbd670c0bfb793c4e6125668e144f9cc326
endokazuki <53686299+endokazuki@users.noreply.github.com>
2021-07-27 19:55:24 +0900

    Add files via upload

--- algorithm/basic/paper.py
+++ algorithm/basic/paper.py
@@ -1,3 +1,13 @@
-array = list(map(int, input().split()))
-print(array)
-#配列の作り方
+a=4/2
+print(int(a))
+
+len=5
+b=[1,2,3,4,5]
+mid=len //2
+c=b[0:]
+#d=b[0:3]
+#配列の頭から３つ要素を抜き出す（0番目から3番目を抜き出すという意味ではない）
+print(c)
+
+d=set(input().split())
+print(d)
